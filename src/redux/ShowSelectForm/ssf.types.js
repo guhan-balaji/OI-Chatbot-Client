928fcee6ad8d976c5befa759566b3c71e539fdd8
unknown <guhanbalaji7@gmail.com>
2020-11-11 11:11:31 +0530

    Modify ViewLogic and MapTable Component. Change SelectForm Component to have connect and mapStateToProps paradigm. Delete ShowSelectForm Folder.(didn't delete in previous commit)

--- src/redux/ShowSelectForm/ssf.types.js
+++
@@ -1,1 +0,0 @@
-export const SHOW_SELECT_FORM = "SHOW_SELECT_FORM";
--- src/redux/ShowSelectForm/ssfActions.js
+++
@@ -1,8 +0,0 @@
-import { SHOW_SELECT_FORM } from "./ssf.types";
-
-export const showSelectForm = (data) => {
-  return {
-    type: SHOW_SELECT_FORM,
-    payload: data,
-  };
-};
